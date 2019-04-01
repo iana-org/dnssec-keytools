94f8c494ceb000aa945664b9618a328bb3075782
Jakob Schlyter <jakob@kirei.se>
2019-04-01 11:00:15 +0200

    add helpers for integrity checking

---
+++ src/kskm/common/integrity.py
@@ -0,0 +1,21 @@
+"""Integrity checking helpers."""
+
+import hashlib
+from typing import Tuple, List
+
+from kskm.tools_common.wordlist import pgp_wordlist
+
+
+def sha2wordlist(message: bytes) -> Tuple[str, List[str]]:
+    """Create SHA-256 hexdigest and word list from bytes."""
+    m = hashlib.new('sha256')
+    m.update(message)
+    hexdigest = m.hexdigest()
+    words = pgp_wordlist(m.digest())
+    return (hexdigest, words)
+
+
+def checksum_bytes2str(message: bytes) -> str:
+    """Format SHA-256 digest."""
+    (hexdigest, words) = sha2wordlist(message)
+    return f"SHA-256 {hexdigest} WORDS {' '.join(words)}"
