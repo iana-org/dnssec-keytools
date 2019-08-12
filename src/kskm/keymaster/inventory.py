a51e7c14bb94363e03a8a3806f07730daed94b2b
Fredrik Thulin <fredrik@thulin.net>
2019-08-12 17:08:21 +0200

    fix printing of keys with same CKA_ID (such as no CKA_ID)
    This software suite does not really use CKA_ID, but rather use
    CKA_LABEL. However, the inventory function was identifying pairs of keys
    using the CKA_ID, which obviously didn't work.
    
    Update the inventory code to use both CKA_ID and CKA_LABEL, which at
    least seems to work fine with keys generated by keymaster, on the AEP
    HSM. Hope they work with keys generated there with the previous software
    too.
    
    updates #29

--- src/kskm/keymaster/inventory.py
+++ src/kskm/keymaster/inventory.py
@@ -1,11 +1,11 @@
 import logging
-from typing import Dict, List
+from typing import Dict, List, Mapping, Tuple
 
 from kskm.common.config import KSKMConfig
 from kskm.common.config_ksk import validate_dnskey_matches_ksk
 from kskm.common.data import FlagsDNSKEY
 from kskm.common.dnssec import public_key_to_dnssec_key
-from kskm.misc.hsm import KSKM_P11, KeyClass
+from kskm.misc.hsm import KSKM_P11, KeyClass, KeyInfo
 
 __author__ = 'ft'
 
@@ -18,14 +18,15 @@ def key_inventory(p11modules: KSKM_P11, config: KSKMConfig) -> List[str]:
     for module in p11modules:
         res += [f'HSM {module.label}:']
         for slot, session in sorted(module.sessions.items()):
-            keys: Dict[KeyClass, dict] = dict()
+            keys: Dict[KeyClass, Dict[str, KeyInfo]] = dict()
             for this in module.get_key_inventory(session):
                 if this.key_class not in keys:
                     keys[this.key_class] = {}
-                if this.key_id in keys[this.key_class]:
-                    logger.error(f'Key with class {this.key_class} and id {this.key_id} already seen in slot')
+                label_and_id = f'{this.label}+{this.key_id}'
+                if label_and_id in keys[this.key_class]:
+                    logger.error(f'Key with class {this.key_class} and label+id {label_and_id} already seen in slot')
                     continue
-                keys[this.key_class][this.key_id] = this
+                keys[this.key_class][label_and_id] = this
 
             formatted = _format_keys(keys, config)
 
@@ -35,13 +36,15 @@ def key_inventory(p11modules: KSKM_P11, config: KSKMConfig) -> List[str]:
     return res
 
 
-def _format_keys(data: dict, config: KSKMConfig) -> List[str]:
+def _format_keys(data: Dict[KeyClass, Dict[str, KeyInfo]], config: KSKMConfig) -> List[str]:
     res: List[str] = []
     pairs: List[str] = []
-    # First, find all pairs (CKA_ID present in both PRIVATE and PUBLIC)
+    # First, find all pairs (CKA_LABEL+CKA_ID present in both PRIVATE and PUBLIC)
     if KeyClass.PUBLIC in data and KeyClass.PRIVATE in data:
-        for key_id in data[KeyClass.PUBLIC].keys():
-            this = data[KeyClass.PUBLIC][key_id]
+        # make copy of keys to be able to remove elements from dict below
+        initial_keylist = list(data[KeyClass.PUBLIC].keys())
+        for label_and_id in initial_keylist:
+            this = data[KeyClass.PUBLIC][label_and_id]
 
             ksk_info = 'Matching KSK not found in configuration'
             # Look for the key in the config
@@ -64,22 +67,31 @@ def _format_keys(data: dict, config: KSKMConfig) -> List[str]:
                     ksk_info = f'KSK \'{ksk.label}/{ksk.description}\', key tag {ksk.key_tag}, ' \
                         f'algorithm={ksk.algorithm.name}'
 
-            if key_id in data[KeyClass.PRIVATE]:
-                pairs += [f'      {this.label:7s} id={this.key_id} {str(this.pubkey)} -- {ksk_info}']
-                data[KeyClass.PRIVATE][key_id] = None
-            data[KeyClass.PUBLIC][key_id] = None
+            if label_and_id in data[KeyClass.PRIVATE]:
+                pairs += [f'      {this.label:7s} {_id_to_str(this.key_id)}{str(this.pubkey)} -- {ksk_info}']
+                del data[KeyClass.PRIVATE][label_and_id]
+            del data[KeyClass.PUBLIC][label_and_id]
     if pairs:
         res += ['    Signing key pairs:'] + pairs
 
     # Now, add all leftover keys
     for cls in data.keys():
-        _res: List[str] = []
-        for key_id in list(data[cls].keys()):
-            this = data[cls][key_id]
-            if this is None:
-                continue
-            _res += [f'      {this.label:7s} id={this.key_id}']
-        if _res:
-            res += [f'    {cls.name} keys:'] + _res
+        _leftovers: List[str] = []
+        for this in list(data[cls].values()):
+            _leftovers += [f'      {this.label:7s} {_id_to_str(this.key_id)}']
+        if _leftovers:
+            res += [f'    {cls.name} keys:'] + _leftovers
 
     return res
+
+
+def _id_to_str(key_id: Tuple[int]) -> str:
+    """
+    CKA_ID is tricky - quite often it is a hex number, like 0x0 but it can also be
+    the CKA_LABEL as a tuple of integers.
+
+    :return: A string suitable for printing (prefixed with 'id=' and suffixed with ' '), or an empty string.
+    """
+    if key_id:
+        return f'id={key_id}'
+    return ''
