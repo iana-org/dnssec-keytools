e6294365286ce8432fc44fd140112da9832e58ac
Jakob Schlyter <jakob@kirei.se>
2019-08-21 22:24:07 +0200

    fix missing main()

--- src/kskm/tools/sha2wordlist.py
+++ src/kskm/tools/sha2wordlist.py
@@ -6,16 +6,23 @@ Command line to to calculate SHA-256 hash of STDIN and present
 result as hex digest and PGP wordlist.
 """
 
+
 import hashlib
 import sys
 
 from kskm.common.wordlist import pgp_wordlist
 
-message = sys.stdin.read().encode()
-m = hashlib.new('sha256')
-m.update(message)
-hexdigest = m.hexdigest()
-words = pgp_wordlist(m.digest())
 
-print(f"SHA-256:    {hexdigest}")
-print(f"PGP Words:  {' '.join(words)}")
+def main():
+    message = sys.stdin.read().encode()
+    m = hashlib.new('sha256')
+    m.update(message)
+    hexdigest = m.hexdigest()
+    words = pgp_wordlist(m.digest())
+    
+    print(f"SHA-256:    {hexdigest}")
+    print(f"PGP Words:  {' '.join(words)}")
+
+
+if __name__ == "__main__":
+    main()
