d6cc244bd1602aadd47cea20dcbffcdca6360413
Fredrik Thulin <fredrik@thulin.net>
2019-09-12 14:15:04 +0200

    refactor signature horizon checking to it's own method
    Now that it is a proper part of ksr-processing.md, it should be in it's
    own function and have start/finish logging like the other checks
    described there.

--- src/kskm/common/config_misc.py
+++ src/kskm/common/config_misc.py
@@ -54,6 +54,7 @@ class RequestPolicy(Policy):
     # TODO: Only have 3 as acceptable key set length, and require special policy for special case?
     acceptable_key_set_lengths: List[int] = field(default_factory=lambda: [2, 3])
     dns_ttl: int = 0  # if this is 0, the config value ksk_policy.ttl will be used instead
+    signature_check_expire_horizon: bool = True
     signature_horizon_days: int = 180
 
     # Verify KSR/SKR chaining
--- src/kskm/ksr/verify_policy.py
+++ src/kskm/ksr/verify_policy.py
@@ -55,24 +55,7 @@ def verify_policy(request: Request, policy: RequestPolicy, logger: Logger) -> No
     check_zsk_policy_algorithm(request, policy, logger)
     check_bundle_overlaps(request, policy, logger)
     check_signature_validity(request, policy, logger)
-
-    dt_now = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
-    for bundle in request.bundles:
-        expire_days = (bundle.expiration - dt_now).days
-        # DPS section 5.1.4: Any RRSIG record generated as a result of a KSK signing operation will not have
-        # a validity period longer than 21 days, and will never expire more than 180 days in the future.
-        if policy.signature_horizon_days and expire_days > policy.signature_horizon_days:
-            logger.error(f'Bundle {bundle.id} signature expires in {expire_days} days ({bundle.expiration}), '
-                        f'above maximum of {policy.signature_horizon_days}')
-            raise KSR_POLICY_SIG_HORIZON_Violation('Bundle signature expire too far into the future')
-
-        # If we're checking that signatures don't expire too long into the future, it makes
-        # sense to also check that they don't expire in the past which could indicate the clock
-        # is wrong on the system performing the checks.
-        if policy.signature_horizon_days > 0 and expire_days < 0:
-            logger.error(f'Bundle {bundle.id} signature expires {abs(expire_days)} days in the past '
-                         f'({bundle.expiration})')
-            raise KSR_PolicyViolation('Bundle signature expire in the past')
+    check_signature_horizon(request, policy, logger)
 
     logger.debug('End "Verify KSR policy parameters"')
 
@@ -141,6 +124,33 @@ def check_signature_validity(request: Request, policy: RequestPolicy, logger: Lo
     _min_str = _fmt_timedelta(request.zsk_policy.min_signature_validity)
     _max_str = _fmt_timedelta(request.zsk_policy.max_signature_validity)
     logger.info(f'KSR-POLICY-SIG-VALIDITY: All {_num_bundles} bundles have {_min_str} <= validity >= {_max_str}')
+
+
+def check_signature_horizon(request, policy, logger):
+    """ Check that signatures do not expire too long into the future. """
+    if not policy.signature_check_expire_horizon:
+        logger.warning('KSR-POLICY-SIG-HORIZON: Disabled by policy (signature_check_expire_horizon)')
+        return
+
+    dt_now = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
+    for bundle in request.bundles:
+        expire_days = (bundle.expiration - dt_now).days
+        # DPS section 5.1.4: Any RRSIG record generated as a result of a KSK signing operation will not have
+        # a validity period longer than 21 days, and will never expire more than 180 days in the future.
+        if policy.signature_horizon_days and expire_days > policy.signature_horizon_days:
+            logger.error(f'Bundle {bundle.id} signature expires in {expire_days} days ({bundle.expiration}), '
+                         f'above maximum of {policy.signature_horizon_days}')
+            raise KSR_POLICY_SIG_HORIZON_Violation('Bundle signature expire too far into the future')
+
+        # If we're checking that signatures don't expire too long into the future, it makes
+        # sense to also check that they don't expire in the past which could indicate the clock
+        # is wrong on the system performing the checks.
+        if policy.signature_horizon_days > 0 and expire_days < 0:
+            logger.error(f'Bundle {bundle.id} signature expires {abs(expire_days)} days in the past '
+                         f'({bundle.expiration})')
+            raise KSR_PolicyViolation('Bundle signature expire in the past')
+
+    logger.info(f'KSR-POLICY-SIG-HORIZON: All signatures expire in less than {policy.signature_horizon_days} days')
 
 
 def check_zsk_policy_algorithm(request: Request, policy: RequestPolicy, logger: Logger) -> None:
