4704979c264dba78f244f48972dfa9559218d6d1
Fredrik Thulin <fredrik@thulin.net>
2019-09-16 16:55:51 +0200

    remove redundant TODO (also in function check_skr_timeline)

--- src/kskm/common/config_misc.py
+++ src/kskm/common/config_misc.py
@@ -60,7 +60,6 @@ class RequestPolicy(Policy):
     # Verify KSR/SKR chaining
     check_chain_keys: bool = True
     check_chain_overlap: bool = True
-    # TODO: match policy timers (should we check PublishSafety and RetireSafety in any way?)
     # TODO: protocol, flags match (check flags on ZSKs to make sure they are ZSKs, not retired etcetera?)
     # TODO: TTL limit (I think the TTL on keys in the KSR is only used to verify the signatures today, is this correct?)
 
