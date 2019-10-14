ccd8e3a7593b791bbf7815be952c05162504f676
Jakob Schlyter <jakob@kirei.se>
2019-10-14 11:48:12 +0200

    add WSGI application stub

---
+++ src/kskm/wksr/app.py
@@ -0,0 +1,11 @@
+import logging
+import yaml
+
+from .server import generate_app
+
+DEFAULT_CONFIG = 'wksr.yaml'
+
+logging.basicConfig(level=logging.INFO)
+
+config = yaml.load(open(DEFAULT_CONFIG).read(), Loader=yaml.SafeLoader)
+application = generate_app(config)
