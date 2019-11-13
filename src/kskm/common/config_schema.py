21e1c3ffc7fd38c1d51099a84da280c46f76c1db
Jakob Schlyter <jakob@kirei.se>
2019-11-13 09:59:59 +0100

    more checks

--- src/kskm/common/config_schema.py
+++ src/kskm/common/config_schema.py
@@ -2,9 +2,17 @@
 
 from datetime import datetime
 
-from voluptuous import (All, Any, Email, IsFile, Match, Range, Required, Schema)
+from voluptuous import All, Any, Email, IsFile, Match, Range, Required, Schema
+from voluptuous.validators import DOMAIN_REGEX
 
 from kskm.common.data import AlgorithmDNSSEC
+from kskm.common.parse_utils import duration_to_timedelta
+
+
+def iso8601_duration():
+    """Validation ISO 8601 durations"""
+    return lambda v: duration_to_timedelta(v)
+
 
 HSM_SCHEMA = Schema({
     Required('module'): str,
@@ -13,13 +21,17 @@ HSM_SCHEMA = Schema({
     'env': dict
 })
 
+KEY_NAME = Match(r'[\w_]+')
+KEY_LABEL = Match(r'[\w_]+')
 RSA_SIZE = All(int, Range(min=1, max=65535))
 RSA_EXPONENT = All(int, Range(min=1))
 HEXDIGEST = Match(r'[0-9a-fA-F]+')
+HOST_NAME = Match(DOMAIN_REGEX)
+DOMAIN_NAME = Any('.', Match('\w+'), Match(DOMAIN_REGEX))
 
 KEY_SCHEMA = Schema({
+    'label': KEY_LABEL,
     'description': str,
-    'label': str,
     'key_tag': All(int, Range(min=0, max=65535)),
     'algorithm': Any(*[x.name for x in AlgorithmDNSSEC]),
     'rsa_size': RSA_SIZE,
@@ -34,7 +46,7 @@ REQUEST_POLICY_SCHEMA = Schema({
 })
 
 REQUEST_POLICY_SCHEMA = REQUEST_POLICY_SCHEMA.extend({
-    'acceptable_domains': Schema([str]),
+    'acceptable_domains': Schema([DOMAIN_NAME]),
     'num_bundles': All(int, Range(min=1)),
     'validate_signatures': bool,
     'keys_match_zsk_policy': bool,
@@ -43,9 +55,9 @@ REQUEST_POLICY_SCHEMA = REQUEST_POLICY_SCHEMA.extend({
     'signature_validity_match_zsk_policy': bool,
     'signature_algorithms_match_zsk_policy': bool,
     'check_keys_match_ksk_operator_policy': bool,
-    'acceptable_key_set_lengths': Schema([int]),
+    'acceptable_key_set_lengths': Schema([All(int, Range(min=1))]),
     'signature_check_expire_horizon': bool,
-    'signature_horizon_days': int,
+    'signature_horizon_days': All(int, Range(min=1)),
     'check_chain_keys': bool,
     'check_chain_overlap': bool,
     'approved_algorithms': Schema([str]),
@@ -59,23 +71,24 @@ RESPONSE_POLICY_SCHEMA = REQUEST_POLICY_SCHEMA.extend({
 })
 
 KSK_POLICY_SCHEMA = Schema({
-    'signers_name': str,
-    'publish_safety': str,
-    'retire_safety': str,
-    'max_signature_validity': str,
-    'min_signature_validity': str,
-    'max_validity_overlap': str,
-    'min_validity_overlap': str,
+    'signers_name': DOMAIN_NAME,
+    'publish_safety': iso8601_duration(),
+    'retire_safety': iso8601_duration(),
+    'max_signature_validity': iso8601_duration(),
+    'min_signature_validity': iso8601_duration(),
+    'max_validity_overlap': iso8601_duration(),
+    'min_validity_overlap': iso8601_duration(),
     'ttl': All(int, Range(min=0)),
 })
 
 SCHEMA_SLOT_SCHEMA = Schema({
-    'publish': Schema(Any(str, Schema([str]))),
-    'sign': Schema(Any(str, Schema([str]))),
-    'revoke': Schema(Any(str, Schema([str]))),
+    'publish': Schema(Any(KEY_NAME, Schema([KEY_NAME]))),
+    'sign': Schema(Any(KEY_NAME, Schema([KEY_NAME]))),
+    'revoke': Schema(Any(KEY_NAME, Schema([KEY_NAME]))),
 })
 
-SCHEMA_SCHEMA = Schema({int: SCHEMA_SLOT_SCHEMA})
+SCHEMA_SLOT = All(int, Range(min=1))
+SCHEMA_SCHEMA = Schema({SCHEMA_SLOT: SCHEMA_SLOT_SCHEMA})
 
 KSRSIGNER_CONFIG_SCHEMA = Schema({
     'hsm': Schema({str: HSM_SCHEMA}),
@@ -84,7 +97,7 @@ KSRSIGNER_CONFIG_SCHEMA = Schema({
         'input_ksr': IsFile(),
         'output_skr': IsFile(),
     },
-    'keys': Schema({str: KEY_SCHEMA}),
+    'keys': Schema({KEY_NAME: KEY_SCHEMA}),
     'request_policy': REQUEST_POLICY_SCHEMA,
     'response_policy': RESPONSE_POLICY_SCHEMA,
     'ksk_policy': KSK_POLICY_SCHEMA,
@@ -115,6 +128,6 @@ WKSR_CONFIG_SCHEMA = Schema({
         'from': Email(),
         'to': Email(),
         'subject': str,
-        'smtp_server': str,
+        'smtp_server': HOST_NAME,
     })
 })
