931405ac7171df59abdd5ea2ec88b69cbda55538
Jakob Schlyter <jakob@kirei.se>
2020-06-17 09:12:48 +0200

    add help and support for multiple files

--- src/kskm/tools/sha2wordlist.py
+++ src/kskm/tools/sha2wordlist.py
@@ -2,20 +2,28 @@
 """
 SHA-256 PGP Words calculator.
 
-Command line to to calculate SHA-256 hash of STDIN and present
-result as hex digest and PGP wordlist.
+Command line to to calculate SHA-256 hash of STDIN and present result as hex
+digest and PGP wordlist. Also able to process one or more files specified on
+the command line.
 """
 
 
+import argparse
 import hashlib
 import sys
+from typing import Optional
 
 from kskm.common.wordlist import pgp_wordlist
 
 
-def main():
+def words(filename: Optional[str] = None) -> None:
+    if filename is not None:
+        print(f"Filename:   {filename}")
+        with open(filename, "rb") as input_file:
+            message = input_file.read()
+    else:
+        message = sys.stdin.buffer.read()
 
-    message = sys.stdin.buffer.read()
     m = hashlib.new("sha256")
     m.update(message)
     hexdigest = m.hexdigest()
@@ -23,6 +31,29 @@ def main():
 
     print(f"SHA-256:    {hexdigest}")
     print(f"PGP Words:  {' '.join(words)}")
+
+    if filename is not None:
+        print()
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="SHA-256 PGP Words Calculator",
+        add_help=True,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "inputs", metavar="filename", type=str, nargs="*", help="Input file"
+    )
+
+    args = parser.parse_args()
+
+    if args.inputs:
+        for filename in args.inputs:
+            words(filename)
+    else:
+        words()
 
 
 if __name__ == "__main__":
