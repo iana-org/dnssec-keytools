c55c2fde432892d650495930f491003954e048df
Jakob Schlyter <jakob@kirei.se>
2020-07-24 20:11:55 +0200

    lint

--- src/kskm/version.py
+++ src/kskm/version.py
@@ -1,12 +1,12 @@
+"""Version information."""
+
 # https://www.python.org/dev/peps/pep-0440
 __version__ = "0.0.1"
 
 try:
     from kskm.buildinfo import __commit__, __timestamp__
-
     __verbose_version__ = f"{__version__} ({__commit__})"
 except (ImportError, ModuleNotFoundError):
     __verbose_version__ = __version__
     __commit__ = None
     __timestamp__ = None
-    pass
