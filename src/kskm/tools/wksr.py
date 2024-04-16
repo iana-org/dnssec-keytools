eb38c9cfc406ad56ca01b01ab9f8797e0116e458
Fredrik Thulin <fredrik@thulin.net>
2024-04-16 09:57:12 +0200

    remove voluptuous

--- src/kskm/tools/wksr.py
+++ src/kskm/tools/wksr.py
@@ -2,14 +2,11 @@
 
 import argparse
 import logging
-import sys
 
-import voluptuous.error
-import voluptuous.humanize
 import yaml
 from werkzeug.serving import run_simple
 
-from kskm.common.config_schema import WKSR_CONFIG_SCHEMA, WKSR_Templates, WKSRConfig
+from kskm.common.config_schema import WKSR_Templates, WKSRConfig
 from kskm.version import __verbose_version__
 from kskm.wksr.peercert import PeerCertWSGIRequestHandler
 from kskm.wksr.server import DEFAULT_TEMPLATES_CONFIG, generate_app, generate_ssl_context
@@ -55,11 +52,11 @@ def main() -> None:
     with open(args.config) as fp:
         _config = yaml.load(fp.read(), Loader=yaml.SafeLoader)
 
-    try:
-        voluptuous.humanize.validate_with_humanized_errors(_config, WKSR_CONFIG_SCHEMA)
-    except voluptuous.error.Error as exc:
-        logging.critical(str(exc))
-        sys.exit(-1)
+    # try:
+    #     voluptuous.humanize.validate_with_humanized_errors(_config, WKSR_CONFIG_SCHEMA)
+    # except voluptuous.error.Error as exc:
+    #     logging.critical(str(exc))
+    #     sys.exit(-1)
 
     config = WKSRConfig(**_config)
     if not config.templates:
