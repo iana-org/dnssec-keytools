dbfb3ffccc7faa081aa887f631242032d59ddd1f
Jakob Schlyter <jakob@kirei.se>
2024-05-14 08:30:34 +0200

    parse config

--- src/kskm/wksr/app.py
+++ src/kskm/wksr/app.py
@@ -4,13 +4,19 @@ import logging
 
 import yaml
 
-from .server import generate_app
+from kskm.common.config_wksr import WKSR_Config, WKSR_Templates
+
+from .server import DEFAULT_TEMPLATES_CONFIG, generate_app
 
 DEFAULT_CONFIG = "wksr.yaml"
 
 logging.basicConfig(level=logging.INFO)
 
 with open(DEFAULT_CONFIG) as fp:
-    config = yaml.load(fp.read(), Loader=yaml.SafeLoader)
+    _config = yaml.safe_load(fp.read())
+
+config = WKSR_Config.from_dict(_config)
+if not config.templates:
+    config.templates = WKSR_Templates.from_dict(DEFAULT_TEMPLATES_CONFIG)
 
 application = generate_app(config)
