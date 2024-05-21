e96d7ac9d2bbbce99ca79ec453beebf6fbe77ceb
Jakob Schlyter <jakob@kirei.se>
2024-05-21 08:09:19 +0200

    remove unused code

--- src/kskm/wksr/app.py
+++ src/kskm/wksr/app.py
@@ -4,9 +4,8 @@ import logging
 
 import yaml
 
-from kskm.common.config_wksr import WKSR_Config, WKSR_Templates
-
-from .server import DEFAULT_TEMPLATES_CONFIG, generate_app
+from kskm.common.config_wksr import WKSR_Config
+from kskm.server import generate_app
 
 DEFAULT_CONFIG = "wksr.yaml"
 
@@ -16,7 +15,5 @@ with open(DEFAULT_CONFIG) as fp:
     _config = yaml.safe_load(fp.read())
 
 config = WKSR_Config.from_dict(_config)
-if not config.templates:
-    config.templates = WKSR_Templates.from_dict(DEFAULT_TEMPLATES_CONFIG)
 
 application = generate_app(config)
